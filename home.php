8637e7ef8f4bbb5588ec2ef00863f8a7c091b11d
trideepshivam <trideepshivam@gmail.com>
2023-11-14 20:08:20 +0530

    open close menu done by menu bar button

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentor'Community</title>
+    <link href="./style/common.css" rel="stylesheet">
+    <link href="./style/signIn.css" rel="stylesheet">
+    <link rel = "icon" href = "https://img.icons8.com/external-basicons-solid-edtgraphics/50/0093ff/external-Teacher-teachers-basicons-solid-edtgraphics-16.png" type = "image/x-icon"> 
+</head>
+<body>
+    <div class="applicationContainer">
+        <div class="topnav">
+            <div id="menuButton">
+                <input type="checkbox" id="menuCloseCheckbox">
+                <label for="menuCloseCheckbox">
+                    <img src="https://img.icons8.com/material-outlined/40/FFFFFF/circled-menu.png" alt="circled-menu"/>
+                </label>
+            </div>
+            <p id="heading">Mentor'Community</p>
+            <div class="profileContainer">
+                <img src="https://img.icons8.com/fluency-systems-filled/40/FFFFFF/user-male-circle.png" alt="user-male-circle"/>
+                <div id="uid">1111</div>
+                <img id="arrowdown" src="https://img.icons8.com/material-outlined/40/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1"/>
+            </div>
+        </div>
+
+        <div class="menuAndContentContainer">
+            <div class="menuContainer">
+                <div class="menu">
+                    <img src="https://img.icons8.com/material-rounded/40/FFFFFF/smart-home-shield.png" alt="smart-home-shield"/>
+                    <p class="menuTxt">Home</p>
+                </div>
+                <div class="menu">
+                    <img src="https://img.icons8.com/ios/40/FFFFFF/performance-macbook.png" alt="performance-macbook"/>
+                    <p class="menuTxt">Dashboard</p>
+                </div>
+                <div class="menu">
+                    <img src="https://img.icons8.com/ios-filled/40/FFFFFF/meeting-time.png" alt="date-woman-woman"/>
+                    <p class="menuTxt">Meet</p>
+                </div>
+                <div class="menu">
+                    <img src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/40/FFFFFF/external-support-advertising-kiranshastry-solid-kiranshastry.png" alt="external-support-advertising-kiranshastry-solid-kiranshastry"/>
+                    <p class="menuTxt">Support</p>
+                </div>
+                <div class="menu">
+                    <img src="https://img.icons8.com/external-glyph-silhouettes-icons-papa-vector/40/FFFFFF/external-Feedback-interface-glyph-silhouettes-icons-papa-vector-2.png" alt="external-Feedback-interface-glyph-silhouettes-icons-papa-vector-2"/>
+                    <p class="menuTxt">Feedback</p>
+                </div>
+                <div class="menu">
+                    <img src="https://img.icons8.com/ios-glyphs/40/FFFFFF/info-squared.png"/>
+                    <p class="menuTxt">About</p>
+                </div>
+            </div>
+            <div class="communityAndSearchContainer">
+                <div  class="searchContainer">
+                    <input type="text" placeholder="Search">
+                </div>
+                <div class="communityContainer">
+
+                </div>
+            </div>
+            <div class="communityDetailAndMsgContainer">
+
+            </div>
+        </div>
+    </div>
+    <script src="./js/application.js"></script>
+</body>
+</html>
